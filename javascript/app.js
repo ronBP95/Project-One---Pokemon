82b7c573745896e5fd16427b3ad6c5b2376e39bb
ronbp <ronmagpantay9513@gmail.com>
2022-02-02 20:22:38 -0800

    made changes to node

--- javascript/app.js
+++ javascript/app.js
@@ -301,3 +301,16 @@ const pokemonEncounter = (min, max) => {
 }
 
 
+// HOSTING //
+
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'content-type': 'text/html' })
+  fs.createReadStream('index.html').pipe(res)
+})
+
+server.listen(process.env.PORT || 3000)
+
+
