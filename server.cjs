5f342a3245c730c655ff85fc18b82e1603899a44
ronbp <ronmagpantay9513@gmail.com>
2022-02-02 21:02:11 -0800

    moved css and js files to public directory for express page serving

--- server.cjs
+++ server.cjs
@@ -1,9 +1,19 @@
 const express = require('express');
+const req = require('express/lib/request');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 8000;
 
 // sendFile will go here
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+// js, assets, css
+app.use(express.static(__dirname + 'public/css'));
+app.use('*/js',express.static('public/javascript'));
+app.use('*/images',express.static('public/assets'));
 
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
